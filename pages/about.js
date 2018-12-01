e1083107e1f2b038e08031cd97fba3cc94874603
sardieri <sardieri@gmail.com>
2018-12-01 22:00:52 +0300

    Simple app implemented

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BTC Rate</h1>
+      <p>Simple app to check current rates of Bitcoin</p>
+    </div>
+  </Layout>
+);
+
+export default About;
